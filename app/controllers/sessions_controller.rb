7119557e26aab3f3ff841c5597d7d5b195e7a3f1
Amar Shah <ashah1129@gmail.com>
2013-05-31 14:50:54 -0500

    5.31 team work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def destroy
+    session["user_id"] = nil
+    redirect_to "/home", notice: "Thanks for shopping!"
+  end
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params["name"])
+
+    if user && user.authenticate(params["password"])
+      session["user_id"] = user.id
+      redirect_to "/home", notice: "Welcome back, #{user.name}"
+    else
+      redirect_to "/login", notice: "Unknown username or password."
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Hugshighfives::Application.routes.draw do
 
+ #Authentication
+get '/login', controller: 'sessions', action: 'new'
+post '/sessions', controller: 'sessions', action: 'create'
+get '/sessions', controller: 'sessions', action: 'create'
+get 'logout', controller:'sessions', action: 'destroy'
+
+
   get "/home", controller: 'homes', action: 'index'
   get "/products", controller: 'products', action: 'index'
+
+
+
 
   end
